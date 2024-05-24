8e5fb784f0ed6162f6dd78406e1b6aed95c51832
Gerito <you@example.com>
2024-05-23 21:54:21 -0500

    Incio, index y style

--- src/main/resources/config/bd.sql
+++ src/main/resources/config/bd.sql
@@ -13,56 +13,64 @@ CREATE TABLE IF NOT EXISTS usuario (
     contrasena VARCHAR(255) NOT NULL,
     PRIMARY KEY(identificacion)
 );
+
 CREATE TABLE IF NOT EXISTS telefonos_usuario (
     id_usuario INT NOT NULL,
     telefono VARCHAR(20) NOT NULL,
     PRIMARY KEY (id_usuario, telefono),
-    FOREIGN KEY (id_usuario)
- REFERENCES usuario(identificacion)
+    FOREIGN KEY (id_usuario) REFERENCES usuario(identificacion)
 );
+
 CREATE TABLE IF NOT EXISTS temporada (
     id_temporada INT AUTO_INCREMENT,
     temporada VARCHAR(255) NOT NULL,
     porcentaje_aumento FLOAT NOT NULL,
-    PRIMARY KEY(id_temporada, temporada),
+    PRIMARY KEY(id_temporada),
     UNIQUE KEY unique_temporada (temporada)
 );
+
 CREATE TABLE IF NOT EXISTS pagos (
     id_pago INT AUTO_INCREMENT,
     fecha_pago DATE NOT NULL,
     monto FLOAT NOT NULL,
     metodo_pago VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id_pago, fecha_pago)
+    PRIMARY KEY(id_pago)
 );
+
 CREATE TABLE IF NOT EXISTS fotografia (
-    id_fotografia INT NOT NULL,
-    fotografia longblob NOT NULL,
+    id_fotografia INT AUTO_INCREMENT,
+    fotografia LONGBLOB NOT NULL,
     PRIMARY KEY(id_fotografia)
 );
+
 CREATE TABLE IF NOT EXISTS region_ubicacion (
-    id_region_ubi INT AUTO_INCREMENT ,
+    id_region_ubi INT AUTO_INCREMENT,
     region VARCHAR(255) NOT NULL,
     PRIMARY KEY(id_region_ubi),
     UNIQUE KEY unique_region (region)
 );
+
 CREATE TABLE IF NOT EXISTS ciudad_ubicacion (
     id_ciudad_ubi INT AUTO_INCREMENT,
     ciudad VARCHAR(255) NOT NULL,
     PRIMARY KEY(id_ciudad_ubi),
     UNIQUE KEY unique_ciudad (ciudad)
 );
+
 CREATE TABLE IF NOT EXISTS pais_ubicacion (
     id_pais_ubi INT AUTO_INCREMENT,
     pais VARCHAR(255) NOT NULL,
     PRIMARY KEY(id_pais_ubi),
     UNIQUE KEY unique_pais (pais)
 );
- CREATE TABLE IF NOT EXISTS tipo_propiedad (
+
+CREATE TABLE IF NOT EXISTS tipo_propiedad (
     id_tipo_propiedad INT AUTO_INCREMENT,
     tipo VARCHAR(55) NOT NULL,
     PRIMARY KEY (id_tipo_propiedad),
     UNIQUE KEY unique_tipo (tipo)
 );
+
 CREATE TABLE IF NOT EXISTS propiedad (
     id_propiedad INT NOT NULL,
     num_habitaciones INT NOT NULL,
@@ -79,18 +87,14 @@ CREATE TABLE IF NOT EXISTS propiedad (
     id_fotografia INT NOT NULL,
     capacidad INT NOT NULL,
     PRIMARY KEY (id_propiedad),
-    FOREIGN KEY (id_tipo_propiedad)
- REFERENCES tipo_propiedad(id_tipo_propiedad),
-    FOREIGN KEY (id_pais_ubi)
- REFERENCES pais_ubicacion(id_pais_ubi),
-    FOREIGN KEY (id_ciudad_ubi)
- REFERENCES ciudad_ubicacion(id_ciudad_ubi),
-    FOREIGN KEY (id_region_ubi)
- REFERENCES region_ubicacion(id_region_ubi),
-    FOREIGN KEY (id_fotografia)
- REFERENCES fotografia(id_fotografia)
+    FOREIGN KEY (id_tipo_propiedad) REFERENCES tipo_propiedad(id_tipo_propiedad),
+    FOREIGN KEY (id_pais_ubi) REFERENCES pais_ubicacion(id_pais_ubi),
+    FOREIGN KEY (id_ciudad_ubi) REFERENCES ciudad_ubicacion(id_ciudad_ubi),
+    FOREIGN KEY (id_region_ubi) REFERENCES region_ubicacion(id_region_ubi),
+    FOREIGN KEY (id_fotografia) REFERENCES fotografia(id_fotografia)
 );
-CREATE TABLE  IF NOT EXISTS reserva (
+
+CREATE TABLE IF NOT EXISTS reserva (
     id_reserva INT AUTO_INCREMENT,
     comentarios_encuesta VARCHAR(255),
     calif_encuesta INT NOT NULL,
@@ -103,12 +107,9 @@ CREATE TABLE  IF NOT EXISTS reserva (
     id_temporada INT NOT NULL,
     id_pago INT NOT NULL,
     PRIMARY KEY(id_reserva),
-    FOREIGN KEY (id_usuario)
- REFERENCES usuario(identificacion),
-    FOREIGN KEY (id_temporada)
- REFERENCES temporada(id_temporada),
-    FOREIGN KEY (id_pago)
- REFERENCES pagos(id_pago)
+    FOREIGN KEY (id_usuario) REFERENCES usuario(identificacion),
+    FOREIGN KEY (id_temporada) REFERENCES temporada(id_temporada),
+    FOREIGN KEY (id_pago) REFERENCES pagos(id_pago)
 );
 
 INSERT INTO region_ubicacion (region) VALUES ('Caribe');
@@ -121,6 +122,8 @@ INSERT INTO ciudad_ubicacion (ciudad) VALUES ('Cali');
 
 INSERT INTO pais_ubicacion (pais) VALUES ('Colombia');
 
+INSERT INTO fotografia (fotografia) VALUES (LOAD_FILE('C://User/Escritorio/imagen.jpg'));
+
 -- Inserta tipo cabaña
 INSERT INTO tipo_propiedad (tipo) VALUES ('Cabaña');
 
@@ -128,7 +131,7 @@ INSERT INTO tipo_propiedad (tipo) VALUES ('Cabaña');
 INSERT INTO tipo_propiedad (tipo) VALUES ('Casa');
 
 INSERT INTO propiedad (id_propiedad, num_habitaciones, precio_base, acepta_mascotas, num_banos, calefaccion, aire_acondicionado, ubicacion_direccion, id_tipo_propiedad, id_pais_ubi, id_ciudad_ubi, id_region_ubi, id_fotografia, capacidad)
-VALUES (1, 3, 150.00, TRUE, 2, TRUE, FALSE, 'Calle Principal 123', 1, 1, 1, 1, 1, 6);
+VALUES (1, 3, 150.00, TRUE, 2, TRUE, FALSE, 'Calle Principal 123', 1, 1, 1, 1, LAST_INSERT_ID(), 6);
 
 INSERT INTO usuario (identificacion, direccion, nacionalidad, nombres, apellidos, email, contrasena)
 VALUES (123456789, 'Calle Principal 123', 'Colombiano', 'Juan', 'Pérez', 'juan@example.com', 'contraseña123');
@@ -140,7 +143,7 @@ INSERT INTO telefonos_usuario (id_usuario, telefono)
 VALUES (123456789, '0987654321'); -- Teléfono para el trabajo
 
 -- Inserta temporada alta
-INSERT INTO temporada (temporada,porcentaje_aumento) VALUES ('Alta', 0.25);
+INSERT INTO temporada (temporada, porcentaje_aumento) VALUES ('Alta', 0.25);
 
 -- Inserta temporada baja
-INSERT INTO temporada (temporada,porcentaje_aumento) VALUES ('Baja', 0.10);
+INSERT INTO temporada (temporada, porcentaje_aumento) VALUES ('Baja', 0.10);
