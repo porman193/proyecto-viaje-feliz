8346d1b4755418d3e4065a8aec2daa205c32f1e5
Gerito <you@example.com>
2024-06-04 04:38:33 -0500

    Funciona boton editar propiedad

--- src/main/java/com/viajefeliza/alquiler/controller/PropertyController.java
+++ src/main/java/com/viajefeliza/alquiler/controller/PropertyController.java
@@ -1,4 +1,3 @@
-
 package com.viajefeliza.alquiler.controller;
 
 import java.util.List;
@@ -7,22 +6,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.viajefeliza.alquiler.model.Property;
 import com.viajefeliza.alquiler.services.PropertyService;
 
 @Controller
-@RequestMapping("/propiedades")
+
 public class PropertyController {
 
     @Autowired
     private PropertyService propertyService;
 
-    @GetMapping("")
+    @GetMapping("/propiedades")
     public String showPropertyForm(Model model) {
         List<Property> properties = propertyService.getAllProperties();
         model.addAttribute("properties", properties);
         return "propiedades/propiedades";
     }
+
+    @GetMapping("/editar/{id}")
+    public String editPropertyForm(@PathVariable Integer id, Model model) {
+        Property property = propertyService.getPropertyById(id);
+        model.addAttribute("property", property);
+        return "propiedades/editar_propiedad"; // Nombre de la vista HTML para editar propiedad
+    }
+    
+    @PostMapping("/editar/{idPropiedad}")
+    public String updateProperty(@PathVariable Integer idPropiedad, @RequestParam Integer numHabitaciones, @RequestParam Float precioBase, @RequestParam(required = false) Boolean aceptaMascotas, @RequestParam Integer numBanos, @RequestParam(required = false) Boolean calefaccion, @RequestParam(required = false) Boolean aireAcondicionado, @RequestParam String ubicacionDireccion, @RequestParam Integer capacidad, @RequestParam String tipoPropiedad, @RequestParam String paisUbicacion, @RequestParam String ciudadUbicacion, @RequestParam String regionUbicacion) {
+        Property property = propertyService.getPropertyById(idPropiedad);
+        property.setNumHabitaciones(numHabitaciones);
+        property.setPrecioBase(precioBase);
+        property.setAceptaMascotas(aceptaMascotas != null && aceptaMascotas);
+        property.setNumBanos(numBanos);
+        property.setCalefaccion(calefaccion != null && calefaccion);
+        property.setAireAcondicionado(aireAcondicionado != null && aireAcondicionado);
+        property.setUbicacionDireccion(ubicacionDireccion);
+        property.setCapacidad(capacidad);
+        property.getTipoPropiedad().setTipo(tipoPropiedad);
+        property.getPaisUbicacion().setPais(paisUbicacion);
+        property.getCiudadUbicacion().setCiudad(ciudadUbicacion);
+        property.getRegionUbicacion().setRegion(regionUbicacion);
+        
+        propertyService.updateProperty(property);
+        return "redirect:/propiedades"; // Redirige a la lista de propiedades después de la actualización
+    }
+    
+      
+    
 }
--- src/main/java/com/viajefeliza/alquiler/model/Property.java
+++ src/main/java/com/viajefeliza/alquiler/model/Property.java
@@ -1,165 +1,172 @@
-package com.viajefeliza.alquiler.model;
+    package com.viajefeliza.alquiler.model;
 
-import jakarta.persistence.*;
+    import java.io.Serializable;
 
-import java.io.Serializable;
+    import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "propiedad")
-public class Property implements Serializable {
+    @Entity
+    @Table(name = "propiedad")
+    public class Property implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_propiedad", nullable = false)
-    private Integer idPropiedad;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id_propiedad", nullable = false)
+        private Integer idPropiedad;
 
-    @Column(name = "num_habitaciones", nullable = false)
-    private Integer numHabitaciones;
+        @Column(name = "num_habitaciones", nullable = false)
+        private Integer numHabitaciones;
 
-    @Column(name = "precio_base", nullable = false)
-    private Float precioBase;
+        @Column(name = "precio_base", nullable = false)
+        private Float precioBase;
 
-    @Column(name = "acepta_mascotas")
-    private Boolean aceptaMascotas;
+        @Column(name = "acepta_mascotas")
+        private Boolean aceptaMascotas;
 
-    @Column(name = "num_banos", nullable = false)
-    private Integer numBanos;
+        @Column(name = "num_banos", nullable = false)
+        private Integer numBanos;
 
-    @Column(name = "calefaccion")
-    private Boolean calefaccion;
+        @Column(name = "calefaccion")
+        private Boolean calefaccion;
 
-    @Column(name = "aire_acondicionado")
-    private Boolean aireAcondicionado;
+        @Column(name = "aire_acondicionado")
+        private Boolean aireAcondicionado;
 
-    @Column(name = "ubicacion_direccion", nullable = false, length = 255)
-    private String ubicacionDireccion;
+        @Column(name = "ubicacion_direccion", nullable = false, length = 255)
+        private String ubicacionDireccion;
 
-    @Column(name = "capacidad", nullable = false)
-    private Integer capacidad;
+        @Column(name = "capacidad", nullable = false)
+        private Integer capacidad;
 
-    @ManyToOne
-    @JoinColumn(name = "id_tipo_propiedad", nullable = false)
-    private TipoPropiedad tipoPropiedad;
+        @ManyToOne
+        @JoinColumn(name = "id_tipo_propiedad", nullable = false)
+        private TipoPropiedad tipoPropiedad;
 
-    @ManyToOne
-    @JoinColumn(name = "id_pais_ubi", nullable = false)
-    private PaisUbicacion paisUbicacion;
+        @ManyToOne
+        @JoinColumn(name = "id_pais_ubi", nullable = false)
+        private PaisUbicacion paisUbicacion;
 
-    @ManyToOne
-    @JoinColumn(name = "id_ciudad_ubi", nullable = false)
-    private CiudadUbicacion ciudadUbicacion;
+        @ManyToOne
+        @JoinColumn(name = "id_ciudad_ubi", nullable = false)
+        private CiudadUbicacion ciudadUbicacion;
 
-    @ManyToOne
-    @JoinColumn(name = "id_region_ubi", nullable = false)
-    private RegionUbicacion regionUbicacion;
+        @ManyToOne
+        @JoinColumn(name = "id_region_ubi", nullable = false)
+        private RegionUbicacion regionUbicacion;
 
 
-    public Integer getIdPropiedad() {
-        return idPropiedad;
+        public Integer getIdPropiedad() {
+            return idPropiedad;
+        }
+
+        public void setIdPropiedad(Integer idPropiedad) {
+            this.idPropiedad = idPropiedad;
+        }
+
+        public Integer getNumHabitaciones() {
+            return numHabitaciones;
+        }
+
+        public void setNumHabitaciones(Integer numHabitaciones) {
+            this.numHabitaciones = numHabitaciones;
+        }
+
+        public Float getPrecioBase() {
+            return precioBase;
+        }
+
+        public void setPrecioBase(Float precioBase) {
+            this.precioBase = precioBase;
+        }
+
+        public Boolean getAceptaMascotas() {
+            return aceptaMascotas;
+        }
+
+        public void setAceptaMascotas(Boolean aceptaMascotas) {
+            this.aceptaMascotas = aceptaMascotas;
+        }
+
+        public Integer getNumBanos() {
+            return numBanos;
+        }
+
+        public void setNumBanos(Integer numBanos) {
+            this.numBanos = numBanos;
+        }
+
+        public Boolean getCalefaccion() {
+            return calefaccion;
+        }
+
+        public void setCalefaccion(Boolean calefaccion) {
+            this.calefaccion = calefaccion;
+        }
+
+        public Boolean getAireAcondicionado() {
+            return aireAcondicionado;
+        }
+
+        public void setAireAcondicionado(Boolean aireAcondicionado) {
+            this.aireAcondicionado = aireAcondicionado;
+        }
+
+        public String getUbicacionDireccion() {
+            return ubicacionDireccion;
+        }
+
+        public void setUbicacionDireccion(String ubicacionDireccion) {
+            this.ubicacionDireccion = ubicacionDireccion;
+        }
+
+        public Integer getCapacidad() {
+            return capacidad;
+        }
+
+        public void setCapacidad(Integer capacidad) {
+            this.capacidad = capacidad;
+        }
+
+        public TipoPropiedad getTipoPropiedad() {
+            return tipoPropiedad;
+        }
+
+        public void setTipoPropiedad(TipoPropiedad tipoPropiedad) {
+            this.tipoPropiedad = tipoPropiedad;
+        }
+
+        public PaisUbicacion getPaisUbicacion() {
+            return paisUbicacion;
+        }
+
+        public void setPaisUbicacion(PaisUbicacion paisUbicacion) {
+            this.paisUbicacion = paisUbicacion;
+        }
+
+        public CiudadUbicacion getCiudadUbicacion() {
+            return ciudadUbicacion;
+        }
+
+        public void setCiudadUbicacion(CiudadUbicacion ciudadUbicacion) {
+            this.ciudadUbicacion = ciudadUbicacion;
+        }
+
+        public RegionUbicacion getRegionUbicacion() {
+            return regionUbicacion;
+        }
+
+        public void setRegionUbicacion(RegionUbicacion regionUbicacion) {
+            this.regionUbicacion = regionUbicacion;
+        }
+
+        public Property() {
+        }
+
+
     }
-
-    public void setIdPropiedad(Integer idPropiedad) {
-        this.idPropiedad = idPropiedad;
-    }
-
-    public Integer getNumHabitaciones() {
-        return numHabitaciones;
-    }
-
-    public void setNumHabitaciones(Integer numHabitaciones) {
-        this.numHabitaciones = numHabitaciones;
-    }
-
-    public Float getPrecioBase() {
-        return precioBase;
-    }
-
-    public void setPrecioBase(Float precioBase) {
-        this.precioBase = precioBase;
-    }
-
-    public Boolean getAceptaMascotas() {
-        return aceptaMascotas;
-    }
-
-    public void setAceptaMascotas(Boolean aceptaMascotas) {
-        this.aceptaMascotas = aceptaMascotas;
-    }
-
-    public Integer getNumBanos() {
-        return numBanos;
-    }
-
-    public void setNumBanos(Integer numBanos) {
-        this.numBanos = numBanos;
-    }
-
-    public Boolean getCalefaccion() {
-        return calefaccion;
-    }
-
-    public void setCalefaccion(Boolean calefaccion) {
-        this.calefaccion = calefaccion;
-    }
-
-    public Boolean getAireAcondicionado() {
-        return aireAcondicionado;
-    }
-
-    public void setAireAcondicionado(Boolean aireAcondicionado) {
-        this.aireAcondicionado = aireAcondicionado;
-    }
-
-    public String getUbicacionDireccion() {
-        return ubicacionDireccion;
-    }
-
-    public void setUbicacionDireccion(String ubicacionDireccion) {
-        this.ubicacionDireccion = ubicacionDireccion;
-    }
-
-    public Integer getCapacidad() {
-        return capacidad;
-    }
-
-    public void setCapacidad(Integer capacidad) {
-        this.capacidad = capacidad;
-    }
-
-    public TipoPropiedad getTipoPropiedad() {
-        return tipoPropiedad;
-    }
-
-    public void setTipoPropiedad(TipoPropiedad tipoPropiedad) {
-        this.tipoPropiedad = tipoPropiedad;
-    }
-
-    public PaisUbicacion getPaisUbicacion() {
-        return paisUbicacion;
-    }
-
-    public void setPaisUbicacion(PaisUbicacion paisUbicacion) {
-        this.paisUbicacion = paisUbicacion;
-    }
-
-    public CiudadUbicacion getCiudadUbicacion() {
-        return ciudadUbicacion;
-    }
-
-    public void setCiudadUbicacion(CiudadUbicacion ciudadUbicacion) {
-        this.ciudadUbicacion = ciudadUbicacion;
-    }
-
-    public RegionUbicacion getRegionUbicacion() {
-        return regionUbicacion;
-    }
-
-    public void setRegionUbicacion(RegionUbicacion regionUbicacion) {
-        this.regionUbicacion = regionUbicacion;
-    }
-
-    public Property() {
-    }
-
-
-}
